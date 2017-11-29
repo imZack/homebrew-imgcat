ed6673d6cc0ac2c623aeedc63583e41ad8ea42bd
YuLun Shih <shih@yulun.me>
2017-11-29 18:28:50 +0800

    Add imgcat.rb

---
+++ imgcat.rb
@@ -0,0 +1,15 @@
+class HomebrewImgcat < Formula
+  desc "iTerm2 is able to display images within the terminal. Using a similar mechanism, it can also facilitate file transfers over any transport (such as ssh or telnet), even in a non-8-bit-clean environment."
+  homepage "https://www.iterm2.com/documentation-images.html"
+  url "https://github.com/imZack/homebrew-imgcat/archive/2017.11.29.tar.gz"
+  sha256 "f5e2900d3efe6b581196b84ef57dd86d8b367ae75eb5b9f940f3acaf36262e82"
+
+  def install
+    bin.install "imgcat"
+  end
+
+  test do
+    test_fixtures("test.png")
+    system "#{bin}/imgcat", "test.png"
+  end
+end
