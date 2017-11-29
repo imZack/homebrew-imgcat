1303cd60542a90deb3ee1da18535182612900187
YuLun Shih <shih@yulun.me>
2017-11-29 18:30:41 +0800

    Fix class name

--- imgcat.rb
+++ imgcat.rb
@@ -1,4 +1,4 @@
-class HomebrewImgcat < Formula
+class Imgcat < Formula
   desc "iTerm2 is able to display images within the terminal. Using a similar mechanism, it can also facilitate file transfers over any transport (such as ssh or telnet), even in a non-8-bit-clean environment."
   homepage "https://www.iterm2.com/documentation-images.html"
   url "https://github.com/imZack/homebrew-imgcat/archive/2017.11.29.tar.gz"
